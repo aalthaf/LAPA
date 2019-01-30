487518dcd8704266b9a8a381afb8a713a45ef996
Althaf Ahamed <althafahamedsadique@cmail.carleton.ca>
2019-01-30 17:43:22 -0500

    reset BCs

--- FDMethod.m
+++ FDMethod.m
@@ -1,23 +1,39 @@
 nx = 20
 ny = 20
 
-maxIterations = 20
+maxIterations = 100
 
 V = zeros (nx,ny)
 V(:,1) = 1
 V(:,ny) = 0
 
-
-for i = 2:nx-1
-    pause (0.5)
-    for j = 2:ny-1
-      V(i,j) =  ( V(i+1,j) - 2* V(i,j) + V(i-1,j) ) + (V(i,j+1) - 2 * V(i,j) + V(i,j-1))
-            
+for n = 1:maxIterations
+    
+    for i = 1:nx-1
+        pause (0.0001)
+        for j = 1:ny
+            V(:,1) = 1
+            V(:,ny) = 0
+            if (j == 1 && i > 1)
+                V(i,j) = (V(i+1,j) - 2*V(i,j) + V(i-1,j) + V(i,j+1) - 2* V(i,j))/3
+                
+                
+            elseif(j ==ny && i > 1 )
+                V(i,j) = (V(i+1,j) - 2* V(i,j) + V(i-1,j) - 2 * V(i,j) + V(i,j-1))/3
+                
+                
+            else
+                if(i>1 && j>1)
+                  V(i,j) =  ((V(i+1,j) - 2* V(i,j) + V(i-1,j) ) + (V(i,j+1) - 2 * V(i,j) + V(i,j-1)))/4
+                end
+            end
+            surf(V)
+        end
+        
+        
     end
-   
-    surf(V,'EdgeColor','none')
+    
 end
 
-
-
-
+    
+    
